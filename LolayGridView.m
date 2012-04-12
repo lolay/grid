4b84aa2cd59bfa925899160bbb1121969e25c874
GaryRudolph <GaryRudolph@mac.com>
2012-04-12 14:36:59 -0700

    Updated license for compliance

--- LolayGridView.m
+++ LolayGridView.m
@@ -1,12 +1,11 @@
 //
-//  Copyright 2010 Daniel Tull. All rights reserved.
 //  Copyright 2012 Lolay, Inc. All rights reserved.
 //
-//  Licensed under the DTGridView License (the "License");
+//  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
 //  You may obtain a copy of the License at
 //
-//    https://raw.github.com/lolay/grid/master/LICENSE
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 //  Unless required by applicable law or agreed to in writing, software
 //  distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,6 +13,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 //
+
 #import "LolayGridView.h"
 #import "LolayGridViewCell.h"
 
